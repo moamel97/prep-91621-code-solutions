e75add82687422c639013502e29e76bf7d9fb0c9
moamel hashim <muamal1997@gmail.com>
2021-09-27 23:44:24 -0700

    completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,18 @@
+var person = {
+  firstName: 'Moamel',
+  lastName: 'Hashim',
+  hobby: 'Playing video games'
+};
+console.log(person);
+
+var fullName = 'Moamel Hashim.';
+
+console.log(fullName);
+
+person.job = 'Trying to survive.';
+
+console.log(person.job);
+
+person.previousJob = 'Flipping burgers at Crusty Crabs.';
+
+console.log(person.previousJob);
